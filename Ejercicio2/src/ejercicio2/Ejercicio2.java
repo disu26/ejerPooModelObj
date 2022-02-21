25ebf86ca569f4fe258d5052dbb7266d52dd2a56
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2022-02-21 14:50:06 -0500

    Segundo commit, Ejercicio 2 completado.
     Changes to be committed:
    	new file:   Ejercicio2/build.xml
    	new file:   Ejercicio2/manifest.mf
    	new file:   Ejercicio2/nbproject/build-impl.xml
    	new file:   Ejercicio2/nbproject/genfiles.properties
    	new file:   Ejercicio2/nbproject/project.properties
    	new file:   Ejercicio2/nbproject/project.xml
    	new file:   Ejercicio2/src/ejercicio2/Ejercicio2.java
    	new file:   Ejercicio2/src/ejercicio2/Main2.java

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,21 @@
+
+package ejercicio2;
+
+/**
+ * Ejecución de un programa que contenga dos metodos main.
+ * 
+ * Se pudo comprobar que:
+ * -Si se ejecuta el programa, por defecto se ejecuta el primer método main.
+ * -El segundo método main se puede ejecutar si se le indica específicamente que 
+ *  se desea ejecutar este.
+ * 
+ * @version 1.00.000 2022-02-21
+ * @author Dímar Andrey Suárez Hidalgo - dimar260212@gmail.com
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Main Principal");
+    }
+    
+}
---
+++ Ejercicio2/src/ejercicio2/Main2.java
@@ -0,0 +1,17 @@
+
+package ejercicio2;
+
+/**
+ * Segundo main.
+ * 
+ * @version 1.00.000 2022-02-21
+ * @author Dímar Andrey Suárez Hidalgo - dimar260212@gmail.com
+ */
+public class Main2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Segundo main");
+    }
+    
+}
